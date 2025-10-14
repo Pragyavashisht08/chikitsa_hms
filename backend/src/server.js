5cf50f2feb6c26a83d43c3d8223b0ebeb1cc692a
Pragyavashisht08 <160639391+Pragyavashisht08@users.noreply.github.com>
2025-10-15 01:24:02 +0530

    Create server.js

---
+++ backend/src/server.js
@@ -0,0 +1,27 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import authRoutes from "./routes/auth.routes.js";
+import patientRoutes from "./routes/patient.routes.js";
+
+dotenv.config();
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// MongoDB connection
+mongoose.connect(process.env.MONGO_URI, {
+  dbName: "hospitalDB"
+})
+.then(() => console.log("✅ MongoDB connected"))
+.catch((err) => console.error("❌ MongoDB connection error:", err));
+
+// Routes
+app.get("/", (req, res) => res.send("CHIKITSA API running"));
+app.use("/api/auth", authRoutes);
+app.use("/api/patients", patientRoutes);
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
